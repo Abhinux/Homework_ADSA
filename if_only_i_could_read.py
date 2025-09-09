eb58dd463658981acf46646a69f68681075aacd7
Abhinux <abhinavsharma8711@gmail.com>
2025-09-09 17:22:09 +0530

    Add files via upload

---
+++ if_only_i_could_read.py
@@ -0,0 +1,16 @@
+t = int(input()) 
+
+for i in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    res=[]
+    for i in range(n-1):
+        if arr[i]>arr[i+1]:
+            res=[i+1,i+2] 
+            found = True
+            break
+
+    if res!=[]:
+        print(res[0],res[1])
+    else:
+        print(-1)
